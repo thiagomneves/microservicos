ba58e1146cc9f5e4ba702392571cea0f947ae44f
Thiago Neves <thiagomn03@gmail.com>
2024-03-08 22:00:52 -0300

    Order done

--- order/src/test/java/com/store/order/OrderApplicationTests.java
+++ order/src/test/java/com/store/order/OrderApplicationTests.java
@@ -1,13 +1,13 @@
-package com.store.order;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class OrderApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.store.order;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class OrderApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
